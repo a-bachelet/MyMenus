61c8d8cf69360b92bc004e67db064a4c224bd43c
Alexis Bachelet <alexis.bachelet@diiage.org>
2020-01-08 21:15:42 +0100

    Added default database context related to entities and changed entities id's relations with guids instead of integers.

---
+++ MyMenus.Context/Migrations/20191222162414_Kernel_Users_Migration.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyMenus.Context.Migrations
+{
+    public partial class Kernel_Users_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    StreetNumber = table.Column<int>(nullable: false),
+                    StreetLetter = table.Column<string>(nullable: true),
+                    StreetFirstLine = table.Column<string>(nullable: false),
+                    StreetSecondLine = table.Column<string>(nullable: false),
+                    ZipCode = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Label = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Lastname = table.Column<string>(nullable: false),
+                    Firstname = table.Column<string>(nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Label = table.Column<string>(nullable: false),
+                    ParentId = table.Column<Guid>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Roles_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Roles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePermissions",
+                columns: table => new
+                {
+                    RoleId = table.Column<Guid>(nullable: false),
+                    PermissionId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePermissions", x => new { x.RoleId, x.PermissionId });
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ParentId",
+                table: "Roles",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AddressId",
+                table: "Users",
+                column: "AddressId",
+                unique: true,
+                filter: "[AddressId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RolePermissions");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
---
+++ MyMenus.Context/MyMenusContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMenus.Entity;
+using System;
+using System.Linq;
+
+namespace MyMenus.Context
+{
+    /// <summary>
+    ///     A database context.
+    /// </summary>
+    public class MyMenusContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<RolePermission> RolePermissions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public MyMenusContext() : base() { }
+
+        public MyMenusContext(DbContextOptions<MyMenusContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+            string dbPass = Environment.GetEnvironmentVariable("DB_PASS");
+            string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+
+            optionsBuilder.UseSqlServer($"Data Source={dbHost};Initial Catalog={dbName};User ID={dbUser};Password={dbPass};");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region ManyToMany
+            
+            modelBuilder
+                .Entity<RolePermission>()
+                .HasKey(rp => new { rp.RoleId, rp.PermissionId  });
+
+            #endregion
+
+            #region ManyToOne
+
+            modelBuilder
+                .Entity<Role>()
+                .HasMany(typeof(Role), "Children")
+                .WithOne("Parent")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            #endregion
+
+            #region OneToOne
+
+            modelBuilder
+                .Entity<User>()
+                .HasOne(typeof(Address), "Address")
+                .WithOne("User")
+                .OnDelete(DeleteBehavior.SetNull);
+
+            #endregion
+        }
+    }
+}
